c3d1960aa76f90f17c026a8d20ed1aa7a87f64bb
l3ryanlopez <looez.2015@gmail.com>
2022-10-29 11:56:07 -0600

    Configurar "Cors" para que nos permita tener solicitudes por elementos cruzados

--- WSVenta/WSVenta/Program.cs
+++ WSVenta/WSVenta/Program.cs
@@ -1,4 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
+string MiCors = "MiCors";
 
 // Add services to the container.
 
@@ -6,6 +7,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MiCors,
+                            builder =>
+                            {
+                                builder.WithOrigins("*");
+                            });
+});
 
 var app = builder.Build();
 
@@ -17,6 +26,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MiCors);
 
 app.UseAuthorization();
 
