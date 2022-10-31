282277e81e1d8852189b2c1812eb3d3ca32ab328
l3ryanlopez <looez.2015@gmail.com>
2022-10-31 14:49:42 -0600

    Se agrega una nueva politica para que permita los metodos PUT Y DELETE

--- WSVenta/WSVenta/Program.cs
+++ WSVenta/WSVenta/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddCors(options =>
                             {
                                 builder.WithHeaders("*");
                                 builder.WithOrigins("*");
+                                builder.WithMethods("*");
                             });
 });
 
