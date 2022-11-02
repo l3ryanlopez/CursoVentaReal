07b4072a81b5689e83ffddd27198035896544d11
l3ryanlopez <looez.2015@gmail.com>
2022-11-01 19:23:16 -0600

    Se crea el modulo para ventas con el uso de transacciones en el backend

---
+++ WSVenta/WSVenta/Controllers/VentaController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WSVenta.Models;
+using WSVenta.Models.Request;
+using WSVenta.Models.Response;
+
+namespace WSVenta.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class VentaController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Add(VentaRequest model)
+        {
+            Respuesta respuesta = new Respuesta();
+
+            try
+            {
+                using (VentaRealContext db = new VentaRealContext())
+                {
+                    using (var transaction = db.Database.BeginTransaction())
+                    {
+                        try
+                        {
+                            var venta = new Ventum();
+                            venta.Total = model.Conceptos.Sum(d => d.Cantidad * d.PrecioUnitario);
+                            venta.Fecha = DateTime.Now;
+                            venta.IdCliente = model.IdCliente;
+                            db.Venta.Add(venta);
+                            db.SaveChanges();
+
+                            foreach (var modelConcepto in model.Conceptos)
+                            {
+                                var concepto = new Models.Concepto();
+                                concepto.Cantidad = modelConcepto.Cantidad;
+                                concepto.IdProducto = modelConcepto.IdProducto;
+                                concepto.PrecioUnitario = modelConcepto.PrecioUnitario;
+                                concepto.Importe = modelConcepto.Importe;
+                                concepto.IdVenta = venta.Id;
+                                db.Conceptos.Add(concepto);
+                                db.SaveChanges();
+                            }
+
+                            transaction.Commit();
+                            respuesta.Exito = 1;
+                            respuesta.Mensaje = "Venta realizada con éxito";
+
+                        }
+                        catch (Exception)
+                        {
+                            transaction.Rollback();
+                            throw;
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                respuesta.Mensaje = ex.Message;
+            }
+
+            return Ok(respuesta);
+        }
+    }
+}
---
+++ WSVenta/WSVenta/Models/Request/VentaRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace WSVenta.Models.Request
+{
+    public class VentaRequest
+    {
+        public int IdCliente { get; set; }
+        public decimal Total { get; set; }
+        public List<Concepto> Conceptos { get; set; }
+    }
+
+    public class Concepto
+    {
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public decimal Importe { get; set; }
+        public int IdProducto { get; set; }
+    }
+
+        
+
+}
