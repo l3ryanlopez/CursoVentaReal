c17c0d93acbd3498e3efe3a0c780af841e1ff247
l3ryanlopez <looez.2015@gmail.com>
2022-11-02 14:45:52 -0600

    validaciones de modelos en el backend MVC .Net

--- WSVenta/WSVenta/Models/Request/VentaRequest.cs
+++ WSVenta/WSVenta/Models/Request/VentaRequest.cs
@@ -1,9 +1,15 @@
-﻿namespace WSVenta.Models.Request
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WSVenta.Models.Request
 {
     public class VentaRequest
     {
+        [Required]
+        [Range(1, Double.MaxValue, ErrorMessage = "El valor del IdCliente debe ser mayor a 0")]
+        [ExistiCliente(ErrorMessage = "El cliente no existe")]
         public int IdCliente { get; set; }
-        public decimal Total { get; set; }
+        [Required]
+        [MinLength(1, ErrorMessage = "Deben existir conceptos")]
         public List<Concepto> Conceptos { get; set; }
     }
 
@@ -15,6 +21,20 @@
         public int IdProducto { get; set; }
     }
 
-        
+    #region Validaciones
+    public class ExistiCliente : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            int idCliente = (int)value;
+            using (var db = new Models.VentaRealContext())
+            {
+                if (db.Clientes.Find(idCliente) == null) return false;
+                
+            }
+            return true;
+        }
+    }
+    #endregion
 
 }
